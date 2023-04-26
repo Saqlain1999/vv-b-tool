11bba12faca5da809fadefeade890d0e0b97e581
Saqlain1999 <saqlain_shah99@hotmail.com>
2023-04-26 21:11:13 +0100

    updated add to cart

--- vb-tool-frontend/src/index.js
+++ vb-tool-frontend/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import Routes from './Routes'
 
-ReactDOM.render(
-    <Routes />,
-    document.getElementById('root')
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Routes />
+);
